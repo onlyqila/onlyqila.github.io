55dd08d66a6d7dd24111825a61bedec56f519658
Rizky Hrlnzyh <120043113+onlyqila@users.noreply.github.com>
2025-09-20 03:07:19 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+    var maxnumhours = 23;
+    var maxnummins = 59;
+    var maxnumsecs = 60;
+    var maxmilisecs = 999;
+    
+    
+    function updateNumbers() {
+    var start_time = 1691452800;
+    var duration = parseInt(Date.now() / 1000 - start_time);
+    
+    var seconds = duration % 60;
+    if (seconds < 10)
+    seconds = "0" + seconds;
+    duration = parseInt(duration / 60);
+    var minutes = duration % 60;
+    if (minutes < 10)
+    minutes = "0" + minutes;
+    duration = parseInt(duration / 60);
+    var hours = duration % 24;
+    if (hours < 10)
+    hours = "0" + hours;
+    duration = parseInt(duration / 24);
+    var days = duration;
+    $('.days-number').text("" + days);
+    $('.seconds').text(hours + " hours " + minutes + " minutes " + seconds + " seconds");
+    setTimeout("updateNumbers()", 1000);
+    }
